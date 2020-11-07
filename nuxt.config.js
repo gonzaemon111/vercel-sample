c7b791ecab54b7c18dbf2f00f5e50c17098adb3b
gonzaemon <goki14914@icloud.com>
2020-11-08 00:15:02 +0900

    change nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,38 +11,21 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
   ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module'
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/buefy
     'nuxt-buefy',
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa'
   ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   }
 }
